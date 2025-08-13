956eeefc0fdf143d5d2003d5d7df974d6de953a3
waldo <waldostrydom5@gmail.com>
2025-08-13 15:41:41 +0200

    init

---
+++ index.js
@@ -0,0 +1,95 @@
+let windowWidth = window.innerWidth
+let pos = 0
+let speed = 0
+let score = 0;
+let miss = 0;
+let end = false;
+const scoreOut = document.getElementById("scoreOut")
+const missOut = document.getElementById("missOut")
+
+
+
+const placeNewBalloon = ()=>{
+    let newBalloon = document.createElement("img")
+    newBalloon.setAttribute("src", "./Assets/Balloon.png")
+    newBalloon.classList.add("balloonPic")
+    newBalloon.classList.add("float")
+
+    let red = Math.floor(Math.random()*255)
+    let green = Math.floor(Math.random()*255)
+    let blue = Math.floor(Math.random()*255)
+
+    newBalloon.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+    newBalloon.style.left=`${pos}px`;
+    newBalloon.style.animation = `floatUp ${speed}s`;
+
+    document.body.appendChild(newBalloon)
+
+    newBalloon.addEventListener("click",()=>{
+       
+        
+        newBalloon.remove()
+        score+=1
+        scoreOut.textContent= `Score: ${score}`
+    })
+}
+
+
+
+const getPos = ()=>{
+let margin = windowWidth*20/100
+let maxWidth = windowWidth-margin
+pos = Math.floor(Math.random()*maxWidth)
+placeNewBalloon()
+}
+
+const getSpeed = ()=>{
+speed=Math.floor(Math.random()*15)+15
+}
+
+const checkMissed = ()=>{
+let balloons = document.querySelectorAll(".float")
+balloons.forEach((e)=>{
+    let balloonPos = e.y
+    if(balloonPos<-20){
+        miss+=1
+        e.remove()
+        missOut.textContent = `Miss: ${miss}`
+    }
+  
+}) 
+}
+
+
+
+const startProgram=setInterval(()=>{
+if(miss<5){    
+checkMissed()    
+getSpeed()
+getPos()
+}else{
+    end= true;
+    endScreen()
+}  
+},3000)
+  
+
+
+
+
+
+
+const endScreen = ()=>{
+    let balloons = document.querySelectorAll(".float")
+    balloons.forEach((e)=>{
+        e.remove()
+    })
+
+    let endMsg = document.createElement("h1")
+    endMsg.textContent = `The End. You Scored ${score}`
+    document.body.appendChild(endMsg)
+
+    clearInterval(startProgram)
+}
+
+
